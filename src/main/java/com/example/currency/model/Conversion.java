ee1d985ba55e8d12cd204421a2f9e5e9dd2e7a7c
dmitr <dmitriylemeosnok@gmail.com>
2024-02-26 11:18:43 +0300

    Removed unnecessary annotations

--- src/main/java/com/example/currency/model/Conversion.java
+++ src/main/java/com/example/currency/model/Conversion.java
@@ -1,12 +1,10 @@
 package com.example.currency.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Data
 @Builder
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class Conversion {
     @JsonProperty("from")
     private String from;
